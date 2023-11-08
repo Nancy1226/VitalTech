e85bf4ee3a3b88aff9380322008aab12f4f68671
Valex4 <yam778123@gmail.com>
2023-11-08 13:30:27 -0600

    [ADD] Graficas

---
+++ src/components/organisms/Measure.jsx
@@ -0,0 +1,14 @@
+import GroupCards from "../molecules/GroupCards";
+import GraphicGroup from "../molecules/GraphicGroup";
+
+function Measure() {
+    return ( 
+        <main>
+            <h1>Graficas de cada uno</h1>
+            <h2>No se muestra nada XD</h2>
+
+        </main>
+     );
+}
+
+export default Measure;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import "../components/styles/Dashboard.css";
+
+import Navbar from "../components/organisms/Navbar";
+import Measure from "../components/organisms/Measure";
+
 function Home() {
     return ( 
-        <>
-        
-        
-        </>
+         <div class="container">
+            <Navbar/>
+            <Measure/>
+        </div>        
      );
 }
 
