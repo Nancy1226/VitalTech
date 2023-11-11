6439543ad22687de59056f74b1bf56e820c6e024
Valex4 <yam778123@gmail.com>
2023-11-11 14:56:48 -0600

    [UPDATE] Graphics and working in the Home component

--- src/components/organisms/BodyDashboard.jsx
+++ src/components/organisms/BodyDashboard.jsx
@@ -27,16 +27,15 @@ function BodyDashboard() {
               />
             </div>
           </div>
-          {/* <!-- End of Analyses --> */}
-
-          {/* <!-- Section graficas --> */}
-
+        
           <div class="new-users">
             <h2>Gráficas</h2>
             <div class="user-list">
              {/* <Graphic/> */}
-             <GraphicGroup graphicName={"Frecuencia Cardiaca"} color={"#367a9d"}/>
-             <GraphicGroup graphicName={"Oxígeno en sangre"} color={"#1865d7"}/>
+             <GraphicGroup graphicName={"Presión arterial"} color1={"#367a9d"} color2={"#5183b5"} columnas={2} variable={"mm Hg"} datakey1={"sistolica"} datakey2={"distolica"}/>
+             <GraphicGroup graphicName={"Temperatura corporal"} color1={"#1865d7"} columnas={1} datakey1={"temp"} variable={"°C"}/>
+             <GraphicGroup graphicName={"Oxígeno en sangre"} color1={"#34ad70"} columnas={1} datakey1={"oxigeno"} variable={"%"}/>
+             <GraphicGroup graphicName={"Frecuencia cardiaca"} color1={"#5819cc"} columnas={1} datakey1={"ppm"} variable={"PPM"}/>
             </div>
           </div>
 
