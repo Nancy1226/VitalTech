fdfb2dd7ad22eaa92b42043a78dc0c08137a2b42
nancy-12_03@hotmail.com <nancy-12_03@hotmail.com>
2023-11-11 20:43:07 -0600

    [ADD Validation email]

--- src/components/organisms/FormRegister.jsx
+++ src/components/organisms/FormRegister.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { Formik, Form, Field } from "formik";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import Title from "../atoms/Title";
 import Label from "../atoms/Label";
 import Img from "../atoms/Img";
@@ -7,20 +10,38 @@ import {images} from '../../images/images.js'
 import GroupInput from "../molecules/GroupInput";
 import Button from "../atoms/Button";
 import "../../components/styles/Forms.css"
+import GroupLink from "../molecules/GroupLink.jsx";
 
 function FormRegister({dato, valor}) {
-    return ( 
-    <>
-<StyledContainer>
+
+  const navigate = useNavigate();
+
+    return (
+      <>
+        <StyledContainer>
           <StyledContainerForm>
             <Formik
               initialValues={{
+                name: " ",
                 email: " ",
                 password: " ",
+                confirmPassword: " ",
               }}
-
-              validate={(valores)=>{ //funcion para validar el forumario
+              validate={(valores) => {
+                //funcion para validar el forumario
                 let errores = {};
+
+                //validacion nombre
+                if (!valores.name) {
+                  errores.name = "Por favor ingresa un nombre";
+                  // } else if (
+                  //   !/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+(?:\s+[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+){1,5}(?<!\s)$/.test(
+                  //     valores.name
+                  //   )
+                  // ) {
+                  //   errores.name =
+                  //     "El nombre solo puede contener letras";
+                }
 
                 //validacion correo
                 if (!valores.email) {
@@ -37,30 +58,95 @@ function FormRegister({dato, valor}) {
                 //validacion contraseña
                 if (!valores.password) {
                   errores.password = "Por favor ingresa una contraseña";
-                } else if (
-                  !/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(
-                    valores.password
-                  )
-                ) {
-                  errores.password =
-                    "La contraseña debe de tener minimo 8 caracteres, maximo 15, al menos una letra mayúscula, al menos una letra minucula, al menos un dígito, No espacios en blanco, al menos 1 caracter especial";
+                 }
+                 if(!valores.confirmPassword){
+                   errores.confirmPassword = "Por favor ingresa una contraseña";
+                 }
+                 
+                 // Validación para confirmar contraseña
+                if (valores.password !== valores.confirmPassword) {
+                  errores.confirmPassword = 'Las contraseñas no coinciden';
                 }
 
                 //validacion para ambos
-                
 
                 return errores;
               }}
+              onSubmit={async (valores, { resetForm }) => {
+                //funcion para enviar el forumario
+                const apiKey = "at_VGPXrkSKUcYgsR9YLKq3up9RGgYCp";
+                const emailAddress = valores.email;
 
-              onSubmit={(valores, {resetForm}) => { //funcion para enviar el forumario
-                resetForm();
-                  console.log(valores)
-                console.log("Formulario enviado");
+                const apiUrl = `https://emailverification.whoisxmlapi.com/api/v3?apiKey=${apiKey}&emailAddress=${emailAddress}`;
+                axios.get(apiUrl).then((response) => {
+                  const responseData = response.data;
+
+                  console.log(
+                    "-----------imprimiendo el response del api correo-------------"
+                  );
+                  console.log(responseData);
+
+                  if (responseData.smtpCheck === "true") {
+                    console.log("La verificación de formato es verdadera");
+
+                    const objectDataFront = {
+                      name: valores.name,
+                      email: valores.email,
+                      password: valores.password
+                    }
+                    
+                    axios
+                      .post("http://localhost:4000/api/signup", objectDataFront)
+                      .then((signupResponse) => {
+                        console.log(
+                          "***********************Viendo el estatus de la API de registro*****************************"
+                        );
+                        console.log(signupResponse.status);
+
+                        setTimeout(() => {
+                          resetForm();
+
+                          if (signupResponse.status === 201) {
+                            Swal.fire({
+                              icon: "success",
+                              title: "Usuario registrado exitosamente",
+                              showConfirmButton: false,
+                              timer: 1500,
+                            });
+                            navigate("/");
+                          }
+                        }, 300);
+                      })
+                      .catch((signupError) => {
+                        console.error(
+                          "Error en la solicitud de registro:",
+                          signupError
+                        );
+                        Swal.fire({
+                          icon: "error",
+                          title: "Oops...",
+                          text: "El correo electronico ya está registrado!",
+                        });
+                      });
+                  } else if (responseData.smtpCheck === "false") {
+                    Swal.fire({
+                      icon: "error",
+                      title: "Oops...",
+                      text: "El correo electrónico no existe!",
+                    });
+                  }
+                });
               }}
             >
-              {({ values, errors, touched,handleSubmit, handleChange, handleBlur }) => (
-
-                  <form onSubmit={handleSubmit}>
+              {({
+                values,
+                errors,
+                touched,
+                handleSubmit,
+                handleChange,
+                handleBlur,
+              }) => (
+                <form onSubmit={handleSubmit}>
                   <Title msn={"Registro"} />
                   <Label
                     txt={"Signos vitales"}
@@ -75,6 +161,18 @@ function FormRegister({dato, valor}) {
                   </StyledContainerIcon>
 
                   <GroupInput
+                    txt={"Nombre"}
+                    type={"name"}
+                    name="name"
+                    dato={values.name}
+                    valor={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.name && errors.name && (
+                    <div className="error">{errors.name}</div>
+                  )}
+
+                  <GroupInput
                     txt={"Correo"}
                     type={"email"}
                     name="email"
@@ -82,7 +180,9 @@ function FormRegister({dato, valor}) {
                     valor={handleChange}
                     onBlur={handleBlur}
                   />
-                {touched.email && errors.email && <div className="error">{errors.email}</div>}
+                  {touched.email && errors.email && (
+                    <div className="error">{errors.email}</div>
+                  )}
 
                   <GroupInput
                     txt={"Contraseña"}
@@ -92,9 +192,30 @@ function FormRegister({dato, valor}) {
                     valor={handleChange}
                     onBlur={handleBlur}
                   />
-                {touched.password && errors.password && <div className="error">{errors.password}</div>}
+                  {touched.password && errors.password && (
+                    <div className="error">{errors.password}</div>
+                  )}
+
+                  <GroupInput
+                    txt={"Confirmar contraseña"}
+                    type={"password"}
+                    name="confirmPassword"
+                    dato={values.confirmPassword}
+                    valor={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.confirmPassword && errors.confirmPassword && (
+                    <div className="error">{errors.confirmPassword}</div>
+                  )}
+
 
                   <Button name={"Registrarse"} />
+
+                  <GroupLink
+                    to={"/"}
+                    txt={"¿Ya tienes una cuenta?"}
+                    msn={"Inicia Sesion"}
+                  />
                 </form>
               )}
             </Formik>
@@ -105,8 +226,6 @@ function FormRegister({dato, valor}) {
           </StyledContainerImg>
         </StyledContainer>
       </>
-    
-    
     );
 }
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,20 +1,43 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import UserContext from '../context/UserContext'
+import RouteProtected from './RouteProtected'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Dashboard from '../pages/Dashboard'
+import NotFound from '../pages/NotFound'
+import FormA from '../components/organisms/FormA'
 
 function App() {
+  const [isLoged, setIsLoged] = useState(
+    () => window.localStorage.getItem("loggedUser") !== null
+  );
+
+  const [userName, setUserName] = useState(
+    () => {
+      const loggedUser = window .localStorage.getItem("loggedUser");
+      return loggedUser ? JSON.parse(loggedUser).nameuser : "";
+    }
+  );
 
   return (
     <>
       <BrowserRouter>
+      <UserContext.Provider value={{ isLoged, setIsLoged, userName, setUserName }}>
         <Routes>
           <Route path="/" element={<Login/>}></Route>
-          <Route path='/home' element={<Home/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
-          <Route path='/dashboard' element={<Dashboard />} ></Route>
+          <Route path='/login' element={<FormA/>}></Route>
+          
+          <Route element={<RouteProtected session={isLoged}/>}>
+            <Route path='/home' element={<Home/>}></Route>
+            <Route path='/dashboard' element={<Dashboard />} ></Route>
+          </Route>
+
+          <Route path="/*" element={<NotFound/>} />
         </Routes>
+      </UserContext.Provider>
       </BrowserRouter>
     </>
   );
